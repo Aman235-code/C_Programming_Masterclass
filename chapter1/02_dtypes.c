0eb0a4b3f8cd5caf6cc1a2a57939e640f9ed366f
Aman Ahamed <amanahamed04gmail.com>
2024-07-18 21:52:06 +0530

    Variables in C

---
+++ chapter1/02_dtypes.c
@@ -0,0 +1,12 @@
+/*
+Author: Aman 
+*/
+#include <stdio.h>
+int main()
+{
+    int a = 1;
+    float b = 1.4;
+    char c = 'd';
+    // this is a comment 
+    return 0;
+}
---
+++ chapter1/03_input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b = 4.5;
+    char c = 'E';
+    c = '\n' // new line 
+    scanf("%d", &a);
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+    return 0;
+}
